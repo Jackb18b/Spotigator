0dcdc1e64611ed734e54a05bfc9880e1bafb5fe8
Jonathon Brown <jackb18b@gmail.com>
2024-04-17 12:58:31 -0400

    in progress file reading

---
+++ Song.h
@@ -0,0 +1,129 @@
+#include <string>
+using namespace std;
+
+struct Style
+{
+    string is_explicit;
+    float danceability;
+    float energy;
+    int key;
+    float loudness;
+    int mode;
+    float speechiness;
+    float acousticness;
+    float instrumentalness;
+    float liveness;
+    float valence;
+    float tempo;
+    int time_signature;
+
+    void setStyle(string is_explicit,
+    float danceability,
+    float energy,
+    int key,
+    float loudness,
+    int mode,
+    float speechiness,
+    float acousticness,
+    float instrumentalness,
+    float liveness,
+    float valence,
+    float tempo,
+    int time_signature) {
+
+    this->is_explicit = is_explicit;
+    this->danceability = danceability;
+    this->energy = energy;
+    this->key = key;
+    this->loudness = loudness;
+    this->mode = mode;
+    this->speechiness = speechiness;
+    this->acousticness = acousticness;
+    this->instrumentalness = instrumentalness;
+    this->liveness = liveness;
+    this->valence = valence;
+    this->tempo = tempo;
+    this->time_signature = time_signature;
+
+    }
+};
+
+class Song
+{
+private:
+    // Columns from Kaggle dataset
+    string spotify_id;
+    string name;
+    string artist;
+    int daily_rank;
+    int daily_movement;
+    int weekly_movement;
+    string country;
+    string snapshot_date;
+    int popularity;
+    int duration_ms;
+    string album_name;
+    string album_release_date;
+
+    Style song_style;
+    
+public:
+    Song(string spotify_id,
+    string name,
+    string artist,
+    int daily_rank,
+    int daily_movement,
+    int weekly_movement,
+    string country,
+    string snapshot_date,
+    int popularity,
+    string is_explicit,
+    int duration_ms,
+    string album_name,
+    string album_release_date,
+    
+    float danceability,
+    float energy,
+    int key,
+    float loudness,
+    int mode,
+    float speechiness,
+    float acousticness,
+    float instrumentalness,
+    float liveness,
+    float valence,
+    float tempo,
+    int time_signature)
+    {
+        song_style.setStyle(is_explicit,
+        danceability,
+        energy,
+        key,
+        loudness,
+        mode,
+        speechiness,
+        acousticness,
+        instrumentalness,
+        liveness,
+        valence,
+        tempo,
+        time_signature);
+
+        this->spotify_id = spotify_id;
+        this->name = name;
+        this->artist = artist;
+        this->daily_rank = daily_rank;
+        this->daily_movement = daily_movement;
+        this->weekly_movement = weekly_movement;
+        this->country = country;
+        this->snapshot_date = snapshot_date;
+        this->popularity = popularity;
+        this->duration_ms = duration_ms;
+        this->album_name = album_name;
+        this->album_release_date = album_release_date;
+
+    }
+
+    
+};
+
--- main.cpp
+++ main.cpp
@@ -1,54 +1,64 @@
 #include <vector>
-#include <iostream>
+#include <string>
 #include <fstream>
+#include <istream>
+#include <sstream>
+#include "Song.h"
 
-struct Song
+
+using namespace std;
+
+
+
+Song readSong(std::string &line)
 {
-    // Columns from Kaggle dataset
-    std::string spotify_id;
-    std::string name;
-    std::string artist;
-    int daily_rank;
-    int daily_movement;
-    int weekly_movement;
-    std::string country;
-    std::string snapshot_date;
-    int popularity;
-    int duration_ms;
-    std::string album_name;
-    std::string album_release_date;
+    vector<string> cols;
+    stringstream ss(line);
 
-    struct Style
+    while(ss.good())
     {
-        bool is_explicit;
-        float danceability;
-        float energy;
-        int key;
-        float loudness;
-        int mode;
-        float speechiness;
-        float acousticness;
-        float instrumentalness;
-        float liveness;
-        float valence;
-        float tempo;
-        int time_signature;
-    };
-};
+        string sub;
+        getline(ss, sub, ',');
+        cols.push_back(sub);
+    }
+    try{
+        Song song(cols[0],cols[1],cols[2],stoi(cols[3]),stoi(cols[4]),
+            stoi(cols[5]),cols[6],cols[7],stoi(cols[8]),cols[9], //9 is bool
+            stoi(cols[10]),cols[11],cols[12],cols[13],cols[14],
+            cols[15],cols[16],cols[17],cols[18],cols[19],
+            cols[20],cols[21],cols[22],cols[23],cols[24]);
+    }
+    catch(exception& exept){
+        return;
+    }
+
+    return song;
+}
 
 
-void fileHandler()
+void fileHandler(std::vector<Song> &songs)
 {
     // Set filePath to downloaded csv file (Change later??)
-    std::string filePath = "";
-    std::fstream csvFile(filePath, std::ios_base::in);
+    string filePath = "C:\\Users\\jackb\\Documents\\UniversityOfFlorida\\Spring2024\\COP3530\\Project3\\universal_top_spotify_songs.csv";
+    fstream csvFile(filePath, std::ios_base::in);
+
+    string line;
+    getline(csvFile, line); // header line
+    
+    while(csvFile.good())
+    {
+        getline(csvFile, line);
+        songs.emplace_back(readSong(line));
+    }
 
 }
 
 
 
+
 int main()
 {
-
+    std::vector<Song> songs;
+    fileHandler(songs);
     return 0;
 }
