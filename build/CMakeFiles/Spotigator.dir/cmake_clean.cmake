b2446c9e5b027deb4c08c45a1ea6d617138cd235
Jonathon Brown <jackb18b@gmail.com>
2024-04-18 18:36:08 -0400

    set up Cmake

---
+++ build/CMakeFiles/Spotigator.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Spotigator.dir/src/Song.cpp.obj"
+  "CMakeFiles/Spotigator.dir/src/Song.cpp.obj.d"
+  "CMakeFiles/Spotigator.dir/src/main.cpp.obj"
+  "CMakeFiles/Spotigator.dir/src/main.cpp.obj.d"
+  "Spotigator.exe"
+  "Spotigator.exe.manifest"
+  "Spotigator.pdb"
+  "libSpotigator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Spotigator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <string>
+#include "Song.h"
+
+
+
+
+
+int main()
+{
+    std::vector<Song> songs;
+    fileHandler(songs, 10000);
+    return 0;
+}
