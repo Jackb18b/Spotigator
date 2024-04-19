422a5fa5d8f0ded654d22a4599bc18f9435f75a4
Jonathon Brown <jackb18b@gmail.com>
2024-04-19 16:43:37 -0400

    added testing

---
+++ test/test.cpp
@@ -0,0 +1,18 @@
+#include <catch2/catch_test_macros.hpp>
+#include <iostream>
+
+// change if you choose to use a different header name
+
+
+using namespace std;
+
+
+// you must write 5 unique, meaningful tests for extra credit on this project!
+
+// See the following for an example of how to easily test your output.
+// This uses C++ "raw strings" and assumes your PageRank outputs a string with
+//   the same thing you print.
+TEST_CASE("Example Test", "[Test]"){
+    
+    REQUIRE(0 == 0);
+}
